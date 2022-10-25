b5a6b758df66b1faf5563a1612d968caa95d3d03
SnowFireWolf <4399kevin11@gmail.com>
2022-10-26 04:50:34 +0800

    update config

--- remix-ts-example/postcss.config.js
+++ remix-ts-example/postcss.config.js
@@ -6,11 +6,13 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
     'postcss-modules': ({
+      // minify class name [name]__[local]___
+      generateScopedName: "[hash:base64:5]",
       getJSON: async (cssFilename, json, outputFilename) => {
         // console.log(cssFilename, json, outputFilename);
         await fsp
           .mkdir(path.dirname(outputFilename), { recursive: true })
-          .catch(() => {});
+          .catch(() => { });
         await fsp.writeFile(
           `${outputFilename.replace(/\.css$/, ".json")}`,
           JSON.stringify(json)
