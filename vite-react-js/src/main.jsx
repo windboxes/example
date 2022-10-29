d13b3b1dfb28615dad22d4a90bf90d1a9b20ec3c
SnowFireWolf <4399kevin11@gmail.com>
2022-10-29 19:15:31 +0800

    vite react js example

---
+++ vite-react-js/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import { TailwindStyledProvider } from "@windboxes/core";
+import twCssModule from "./styles/tailwind.module.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TailwindStyledProvider tailwind={twCssModule}>
+      <App />
+    </TailwindStyledProvider>
+  </React.StrictMode>
+);
