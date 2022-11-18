47cdc8d0e2aca3d3aacf45303fd741400c017d09
Kevin Zheng <4399kevin11@gmail.com>
2022-11-18 23:42:40 +0800

    update qwik example

--- qwik/qwik-ts/src/routes/flower/index.tsx
+++ qwik/qwik-ts/src/routes/flower/index.tsx
@@ -1,8 +1,14 @@
 import { component$, useClientEffect$, useStore, useStylesScoped$ } from '@builder.io/qwik';
 import { DocumentHead, useLocation } from '@builder.io/qwik-city';
+import styled from '~/windboxes';
 import styles from './flower.css?inline';
 
+
+
 export default component$(() => {
+  const containerClasses = styled('container mx-auto h-screen flex flex-col justify-center items-center');
+  const cardClasses = styled('p-5 w-full bg-gray-50 rounded-xl border border-gray-200');
+
   useStylesScoped$(styles);
   const loc = useLocation();
 
@@ -20,36 +26,38 @@ export default component$(() => {
   });
 
   return (
-    <>
-      <input
-        type="range"
-        value={state.number}
-        max={50}
-        onInput$={(ev) => {
-          state.number = (ev.target as HTMLInputElement).valueAsNumber;
-        }}
-      />
-      <div
-        style={{
-          '--state': `${state.count * 0.1}`,
-        }}
-        class={{
-          host: true,
-          pride: loc.query['pride'] === 'true',
-        }}
-      >
-        {Array.from({ length: state.number }, (_, i) => (
-          <div
-            key={i}
-            class={{
-              square: true,
-              odd: i % 2 === 0,
-            }}
-            style={{ '--index': `${i + 1}` }}
-          />
-        )).reverse()}
+    <div class={containerClasses}>
+      <div class={cardClasses}>
+        <input
+          type="range"
+          value={state.number}
+          max={50}
+          onInput$={(ev) => {
+            state.number = (ev.target as HTMLInputElement).valueAsNumber;
+          }}
+        />
+        <div
+          style={{
+            '--state': `${state.count * 0.1}`,
+          }}
+          class={{
+            host: true,
+            pride: loc.query['pride'] === 'true',
+          }}
+        >
+          {Array.from({ length: state.number }, (_, i) => (
+            <div
+              key={i}
+              class={{
+                square: true,
+                odd: i % 2 === 0,
+              }}
+              style={{ '--index': `${i + 1}` }}
+            />
+          )).reverse()}
+        </div>
       </div>
-    </>
+    </div>
   );
 });
 
--- qwik/qwik-ts/src/windboxes.ts
+++ qwik/qwik-ts/src/windboxes.ts
@@ -1,9 +1,14 @@
 import { parseStyle } from '@windboxes/core';
-
 import tailwind from './tailwind.module.css';
 
 
 
+interface CSSModules {
+  [key: string]: string;
+}
+
+
+
 export default function styled(styles: string | string[]) {
-  return parseStyle(styles, tailwind as any);
+  return parseStyle(styles, tailwind as CSSModules);
 }
